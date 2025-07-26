b130740aa839ce5cb3f093976e64587d7c20f83b
Kevin Nguyen <kevinn24@uw.edu>
2025-07-26 02:24:53 -0700

    finish task 5a, need to add math definitions to comments

--- src/designs.ts
+++ src/designs.ts
@@ -1,5 +1,5 @@
-import { Pattern, Theme } from './pattern';
-import { nil } from './list';
+import { Block, Pattern, Theme } from './pattern';
+import { cons, nil } from './list';
 
 
 /**
@@ -12,7 +12,12 @@ import { nil } from './list';
  *
  */
 export const slopes = (rows: bigint, theme: Theme): Pattern => {
-  return nil; // TODO: replace
+  // TODO
+  if (rows == 0n) {
+    return nil;
+  }
+  const b: Block  = {form: "lines", theme: theme, direction: "R"};
+  return cons(cons(b, cons(b, nil)), slopes(rows-1n, theme));
 }
 
 /**
@@ -25,7 +30,19 @@ export const slopes = (rows: bigint, theme: Theme): Pattern => {
  *
  */
 export const diamonds = (rows: bigint, theme: Theme): Pattern => {
-  return nil; // TODO: replace
+  // TODO
+  if (rows == 0n) {
+    return nil;
+  }
+  const b1: Block = { form: "lines", theme: theme, direction: "R" };
+  const b2: Block = { form: "lines", theme: theme, direction: "L" };
+  const b3: Block = { form: "lines", theme: theme, direction: "L" };
+  const b4: Block = { form: "lines", theme: theme, direction: "R" };
+
+  const row1 = cons(b1, cons(b2, nil));
+  const row2 = cons(b3, cons(b4, nil));
+  
+  return cons(row1, cons(row2, diamonds(rows - 2n, theme)));
 }
 
 /**
@@ -37,8 +54,20 @@ export const diamonds = (rows: bigint, theme: Theme): Pattern => {
  *  TODO: copy your "hearts" math definition from 3b) here!
  *
  */
-export const hearts = (rows: bigint, theme: Theme): Pattern => {
-  return nil; // TODO: replace
+export const hearts = (rows: bigint, theme: Theme): Pattern => { 
+  // TODO
+  if (rows == 0n) {
+    return nil;
+  }
+  const b1: Block = {form: "waves", theme: theme, direction: "TL"};
+  const b2: Block = {form: "waves", theme: theme, direction: "TR"};
+  const b3: Block = {form: "lines", theme: theme, direction: "R"};
+  const b4: Block = {form: "lines", theme: theme, direction: "L"};
+
+  const row1 = cons(b1, cons(b2, nil));
+  const row2 = cons(b3, cons(b4, nil));
+
+  return cons(row1, cons(row2, hearts(rows-2n, theme)));
 }
 
 /**
@@ -51,5 +80,18 @@ export const hearts = (rows: bigint, theme: Theme): Pattern => {
  *
  */
 export const infinities = (rows: bigint, theme: Theme): Pattern => {
-  return nil; // TODO: replace
+  // TODO
+  if (rows == 0n) {
+    return nil;
+  }
+  const b1: Block = {form: "waves", theme: theme, direction: "TL"};
+  const b2: Block = {form: "waves", theme: theme, direction: "TR"};
+  const b3: Block = {form: "waves", theme: theme, direction: "BL"};
+  const b4: Block = {form: "waves", theme: theme, direction: "BR"};
+
+  const row1 = cons(b1, cons(b2, nil));
+  const row2 = cons(b3, cons(b4, nil));
+
+  return cons(row1, cons(row2, infinities(rows-2n, theme)));
+
 }
